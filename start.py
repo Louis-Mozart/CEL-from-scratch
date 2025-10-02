9c7a43bcea597edb9e73b2e6dacc937b88cf0dec
Louis Mozart KAMDEM TEYOU <93260304+Louis-Mozart@users.noreply.github.com>
2025-10-02 15:36:31 +0200

    Update start.py

--- start.py
+++ start.py
@@ -29,7 +29,7 @@ def learn_class_expression(pos_examples, neg_examples):
                 if all(expr_func(p) for p in pos_examples) and not any(expr_func(n) for n in neg_examples):
                     candidate_expressions.append(expr)
     
-    # Return the simplest expression (smallest number of constraints)
+    # Return the simplest expression
     return min(candidate_expressions, key=lambda e: len(e), default=None)
 
 # Learn an expression for the given examples
